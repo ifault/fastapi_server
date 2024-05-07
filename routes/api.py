a221e1477d376abdd9443f958d43646f9abef2b7
binsu2 <binsu2@cisco.com>
2024-05-07 11:36:43 +0800

    add git ignore

--- routes/api.py
+++ routes/api.py
@@ -16,4 +16,4 @@ async def login(form: LoginData):
         user.access_token = access_token
         await user.save()
         return JSONResponse(content={"token": access_token, "success": True, "message": ""}, status_code=200)
-    return JSONResponse(content={"token": "", "success": False, "message": "用户不存在"}, status_code=200)
+    return JSONResponse(content={"token": "", "success": False, "message": "用户名密码错误"}, status_code=200)
