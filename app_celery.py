bd43dfba1d0e3ce6e89ba16cab4bbc721b05b322
ifault <international.fault@gmail.com>
2024-06-18 18:52:30 +0800

    update docker compose file

--- app_celery.py
+++ app_celery.py
@@ -5,26 +5,26 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#
-# def create_app():
-#     host = os.getenv("POSTGRES_HOST")
-#     port = os.getenv("DATABASE_PORT")
-#     name = os.getenv("DATABASE_NAME")
-#     user = os.getenv("DATABASE_USER")
-#     pass_ = os.getenv("DATABASE_PASSWORD")
-#     r_host = os.getenv("REDIS_HOST")
-#     r_port = os.getenv("REDIS_PORT")
-#     celery_ = Celery(
-#         __name__,
-#         broker=f'redis://{r_host}:{r_port}/0',
-#         backend=F"db+postgresql://{user}:{pass_}@{host}:{port}/{name}",
-#         task_ignore_result=False,
-#         include=['tasks.morning']
-#     )
-#     return celery_
+
+def create_app():
+    host = os.getenv("POSTGRES_HOST")
+    port = os.getenv("DATABASE_PORT")
+    name = os.getenv("DATABASE_NAME")
+    user = os.getenv("DATABASE_USER")
+    pass_ = os.getenv("DATABASE_PASSWORD")
+    r_host = os.getenv("REDIS_HOST")
+    r_port = os.getenv("REDIS_PORT")
+    celery_ = Celery(
+        __name__,
+        broker=f'redis://{r_host}:{r_port}/0',
+        backend=F"db+postgresql://{user}:{pass_}@{host}:{port}/{name}",
+        task_ignore_result=False,
+        include=['tasks.morning']
+    )
+    return celery_
 
 
-# celery = create_app()
+celery = create_app()
 # celery.conf.update(
 #     CELERYBEAT_SCHEDULE={
 #     },
