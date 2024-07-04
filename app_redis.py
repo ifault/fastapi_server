889d74aa3e532cb8a5beaf2a7de43f613918e8e5
ifault <international.fault@gmail.com>
2024-07-04 16:30:40 +0800

    update docker compose file

--- app_redis.py
+++ app_redis.py
@@ -5,8 +5,7 @@ load_dotenv()
 
 async def init_redis_pool() -> redis.Redis:
     redis_c = await redis.from_url(
-        "redis://host.docker.internal:6379/2",
-        # "redis://host.docker.internal:6379/2",
+        "redis://host.docker.internal:6379",
         encoding="utf-8",
         db=2,
         decode_responses=True,
