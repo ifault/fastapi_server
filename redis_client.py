7f8a199ebaf29d7ebeb57e1fbbdfdd975b873124
ifault <international.fault@gmail.com>
2024-07-04 16:45:10 +0800

    update docker compose file

--- redis_client.py
+++ redis_client.py
@@ -1,9 +1,16 @@
+import os
+
 import redis as rr
+from dotenv import load_dotenv
+
+load_dotenv()
 _redis_client = None
 
 
 def get_redis_client():
     global _redis_client
+    r_host = os.getenv("REDIS_HOST")
+    r_port = os.getenv("REDIS_PORT")
     if _redis_client is None:
-        _redis_client = rr.from_url("redis://host.docker.internal:6379", db=2, decode_responses=True)
+        _redis_client = rr.from_url(f"redis://{r_host}:{r_port}", db=2, decode_responses=True)
     return _redis_client
